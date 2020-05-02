217b715022f1b8d08b9a4a202055fc35a5665cfc
yunsmre <emrekoc@tutamail.com>
2020-05-02 02:25:21 +0200

    added hog example.

---
+++ API/hog(working_fine).py
@@ -0,0 +1,30 @@
+"""
+Implemets histogram of gradients on given image.
+Taken from internet, source information needs to be added.
+"""
+
+
+import matplotlib.pyplot as plt
+import cv2 as cv
+from skimage.feature import hog
+from skimage import data, exposure
+
+
+image = cv.imread("iron-nut-250x250.jpeg")
+
+fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
+                    cells_per_block=(1, 1), visualize=True, multichannel=True)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+
+ax1.axis('off')
+ax1.imshow(image, cmap=plt.cm.gray)
+ax1.set_title('Input image')
+
+# Rescale histogram for better display
+hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+
+ax2.axis('off')
+ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+ax2.set_title('Histogram of Oriented Gradients')
+plt.show()
